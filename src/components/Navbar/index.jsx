29653e932596636de561d9d2b8b407e7b307b4f3
Abhinav <amj16122004@gmail.com>
2025-03-19 22:22:50 +0530

    commiting

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png';
 export const Navbar = () => {
     return(
         <header className='header'>
-            <img src={logo} className='logo' />
+            <img src={logo} alt="Website icon" className='logo' />
             <h1 className='title'>NoteIt</h1>
         </header>
     )
